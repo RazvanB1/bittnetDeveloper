2fb0466c797a27dbd26f59e5ded29dda179ec86d
RazvanB1 <45684382+RazvanB1@users.noreply.github.com>
2022-10-30 13:27:44 +0200

    Test 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -43,6 +43,7 @@ namespace MyNameSpace
                 Console.ReadKey();
                 Console.ReadKey();
                 Console.ReadKey();
+                Console.ReadKey();
             }
         }
     }
